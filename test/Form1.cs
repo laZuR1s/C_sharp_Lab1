e8464f645490d84444748634af066beacdc5799b
Александр <sashalazyr@gmail.com>
2024-09-08 15:50:59 +0300

    Добавьте файлы проекта.

---
+++ test/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private int clickCount = 0;
+        private string[] icons = { "C:\\Users\\sasha\\Downloads\\Papirus-Team-Papirus-Apps-Youtube.ico", "C:\\Users\\sasha\\Downloads\\Froyoshark-Enkel-Telegram.ico",
+            "C:\\Users\\sasha\\Downloads\\Papirus-Team-Papirus-Apps-Gnome-twitch.ico" };
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Начальная форма";
+            this.BackColor = Color.Aqua;
+            this.Size = new Size(1000, 1000);
+            this.Icon = new Icon("C:\\Users\\sasha\\Downloads\\Papirus-Team-Papirus-Apps-Youtube.ico");
+            this.MouseClick += new MouseEventHandler(Form1_MouseClick);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+           clickCount=(clickCount+1)%icons.Length;
+            this.Icon = new Icon(icons[clickCount]);
+
+          string iconName=icons[clickCount].Split('.')[0];
+            this.Text = iconName;
+        }
+    }
+}
